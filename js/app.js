a9b78f1b3669e9b3dc8ec778bd1c85bc821168ed
Ryan Nell <ryannell@hotmail.com>
2018-07-12 12:56:20 +0100

    Update submit function

--- js/app.js
+++ js/app.js
@@ -1,28 +1,4 @@
-// Select color input
-// Select size input
 
-//Declaring variables
-const formData = document.querySelector('form');
-
-
-//form submit function
-formData.submit(function(event){
-
-  const tableRow = document.querySelector('tr');
-  const tableData = document.querySelector('td');
-
-  //Remove current rows and columns when the form uis sumbitted
-  tableRow.remove();
-  tableData.remove();
-
-  //call function to make the grid
-  makeGrid();
-  event.preventDefault();
-});
-
-
-function makeGrid() {
-
-  
-
-}
+//Select button element and add a click listener to it.
+const submitButton = document.querySelector('.submit-button');
+submitButton.addEventListener('click', makeGrid);
